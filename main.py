3f12ceeeb370c8924570f5a1c5de5b1a4c35c52c
root <gingggg@gmail.com>
2021-05-16 05:41:58 +0000

    jsonb_fetch_test

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import ezdxf
 from db import con
 
-res = con.execute('select 1')
-print(res.fetchall())
+res = con.execute('''select jsonb_agg(st_asgeojson(st_transform(geometry, 5186))::jsonb) from seoul_jijuk_0501_4326 where "A1" in ('1168010600109450010')''')
+data = res.fetchall()
+print(type(data))
+print(type(data[0]))
+print(type(data[0]['jsonb_agg'][0]))
 
 
