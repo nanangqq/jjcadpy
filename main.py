dad6724b21bb9568e6a9935f5e73c2007f24ecad
nanangqq <gingggg@gmail.com>
2021-05-17 08:49:49 +0900

    pnu리스트로 geojson feature 받아옴

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ import ezdxf
 from db import con
 
 params = json.loads(argv[1])
-print(params)
+# print(params['body'][0])
+pnus = params['body']
 
-res = con.execute('''select jsonb_agg(st_asgeojson(st_transform(geometry, 5186))::jsonb) from seoul_jijuk_0501_4326 where "A1" in ('1168010600109450010')''')
+res = con.execute('''
+select jsonb_agg(st_asgeojson(st_transform(geometry, 5186))::jsonb) 
+from seoul_jijuk_0501_4326 
+where "A1" in (%s)'''%','.join(["'%s'"%pnu for pnu in pnus]))
 data = res.fetchall()
 # print(type(data))
 # print(type(data[0]))
 # print(type(data[0]['jsonb_agg']))
+features = data[0]['jsonb_agg']
+print(features)
 
-
+doc = ezdxf.new()
